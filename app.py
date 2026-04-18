ab40b8eb2de8f69a47a496fdcade2937d9944c57
adnankhan-eng378 <adnankhan48089@gmail.com>
2026-04-18 16:29:52 +0500

    rollback add in this file A3rd d time

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello DevOps World IAM ADNAN KHAN  add rollback in this AJ project 🚀"
+    return "Hello DevOps World IAM ADNAN KHAN  add rollback in  hhhhnh this AJ project 🚀"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
